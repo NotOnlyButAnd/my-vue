addd1cf2770185302576aab41d03e30d79c6f580
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-04-18 02:40:17 +0300

    First Steps
    Создал первое приложение Vue JS
    
    Создал два компонента - ArticleList и Article. Сделал вывод, но пока некрасивый (а может так и оставить)

---
+++ my-first-app/src/components/Article.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="article">
+        <li>
+            <h1>
+                ID: {{ id }}        
+            </h1>
+            <h1 v-if="title">
+                {{ title }}        
+            </h1>
+            <p v-if="body">
+                {{ body }}        
+            </p>
+        </li>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ArticleItem',
+  props: {
+    msg: String,
+    title: String,
+    body: String,
+    id: Number
+  }
+}
+</script>
+
+<style scoped>
+.article{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+h1{
+    color: #2c503a;
+    font-size: 1em;
+}
+</style>
---
+++ my-first-app/src/components/ArticleList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="articleList">
+        <h1> {{ msg }} </h1> 
+        <ul v-if="articles.length">
+            <Article
+                v-for="article in articles"
+                v-bind:key="article.id"
+                v-bind:id="article.id"
+                v-bind:title="article.title"
+                v-bind:body="article.body"
+                
+            >
+            </Article>
+        </ul>
+        <p v-else>
+            No articles in list
+        </p>
+  </div>
+</template>
+
+<script>
+
+import Article from './Article.vue'
+
+export default {
+  name: 'ArticleList',
+  props: {
+    msg: String
+  },
+  components:{
+      Article
+  },
+  data() {
+      return{
+          articles: [
+              {
+                  id: 1,
+                  title: 'TITLE 1',
+                  body: 'Body 1 - Learn vue'
+              },
+              {
+                  id: 2,
+                  title: 'TITLE 2',
+                  body: 'Body 2 - Learn about single-file components'
+              },
+              {
+                  id: 3,
+                  title: 'TITLE 3',
+                  body: 'Body 3 - Fall in love'
+              }
+          ]
+      }
+  }
+
+}
+</script>
+
+<style>
+#articleList {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
